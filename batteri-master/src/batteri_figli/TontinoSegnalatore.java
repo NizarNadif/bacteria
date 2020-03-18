242ddfbd5608bacd1d8054e9e9ca36367d69dacb
karanbir-singh <karans1723@gmail.com>
2020-03-18 21:38:34 +0100

    Inutili commenti eliminati

--- batteri-master/src/batteri_figli/TontinoSegnalatore.java
+++ batteri-master/src/batteri_figli/TontinoSegnalatore.java
@@ -114,9 +114,6 @@ public class TontinoSegnalatore extends batteri.Batterio implements Cloneable {
 
     @Override
     protected void Sposta() {
-
-        //Controlla nel 7x7 attorno a lui se c'è del cibo e tiene in memoria la prima posizione in cui l'ha trovato e l'ultima
-        //Il batterio segnalerà agli altri
         switch (situazione) {
             case 0: {
                 boolean risultato = controlloVicini(5);
